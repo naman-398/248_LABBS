5f3b1367d6ee8cffc73d7bd88799e0276995918e
naman-398 <you@example.com>
2024-03-18 15:15:37 +0530

    navbar-change2

--- src/components/Navbarpage.jsx
+++ src/components/Navbarpage.jsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react'
 import { Container } from 'react-bootstrap'
 import pagelogo from "../Assets/Images/pagelogo.webp";
 import Commonbtn from '../Commonbtn/Commonbtn';
+import { Navcross, Navline } from '../Commonsvg/Icon';
 const Navbarpage = () => {
     const [first, setfirst] = useState(false)
     function show() {
@@ -19,17 +20,16 @@ const Navbarpage = () => {
         <Container className=' py-4  '>
             <div className=' d-flex  align-items-center  justify-content-between '>
 <img src={pagelogo} alt="" className=' navimg cursor-pointer' />
-<ul className={`${first ? "nav-true" : "nav-false"} d-flex  align-items-center  mb-0  ps-0 gap-4  mobile-view `}>
+<div className= "d-flex  align-items-center  mb-0  ps-0 gap-4 " >
+<ul className={`${first ? "nav-true" : "nav-false"} d-flex  align-items-center  mb-0  ps-0 gap-4  mobile-view`}>
     <li><a href="#aboutus" onClick={show} className=' opacity-70 fs-1sm fw-normal  lh24 text-white  mb-0  text-decoration-none pagelinks position-relative '>About Us</a></li>
     <li><a href="#team" onClick={show} className=' opacity-70 fs-1sm fw-normal  lh24 text-white  mb-0  text-decoration-none pagelinks position-relative  '>Our Team</a></li>
     <li><a href="#rm" onClick={show} className=' opacity-70 fs-1sm fw-normal  lh24 text-white  mb-0  text-decoration-none pagelinks  position-relative '>Roadmap</a></li>
-    <li className=' ps-3 cursor-pointer '><Commonbtn text='Contact Us'/></li>
+    <li className=' ps-3 cursor-pointer d-block d-sm-none  d-lg-block'><Commonbtn text='Contact Us'/></li>
 </ul>
-<span onClick={show} className=' d-block d-md-none position-relative z-11'>{first === false ? <svg xmlns="http://www.w3.org/2000/svg" width="40" height="30" fill="#FFFFFF" class="bi bi-list" viewBox="0 0 16 16">
-  <path fill-rule="evenodd" d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5"/>
-</svg> : <svg xmlns="http://www.w3.org/2000/svg" width="40" height="30" fill="#FFFFFF" class="bi bi-x-lg" viewBox="0 0 16 16">
-  <path d="M2.146 2.854a.5.5 0 1 1 .708-.708L8 7.293l5.146-5.147a.5.5 0 0 1 .708.708L8.707 8l5.147 5.146a.5.5 0 0 1-.708.708L8 8.707l-5.146 5.147a.5.5 0 0 1-.708-.708L7.293 8z"/>
-</svg>}</span>
+<button className=' ps-3 cursor-pointer d-none d-sm-block d-lg-none bg-transparent border-0 '><Commonbtn text='Contact Us'/></button>
+</div>
+<span onClick={show} className=' d-block d-lg-none position-relative z-11'>{first === false ? <Navline/>: <Navcross/>}</span>
 </div>
         </Container>
 
